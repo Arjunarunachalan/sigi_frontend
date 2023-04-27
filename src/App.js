b7098178654ddafd64f6c362f7f8d2118d73e153
Arjun <arju04551@gmail.com>
2023-04-27 14:44:40 +0530

    added validation in signup form and added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import './assets/css/global.css'
 import Signup from './Components/Signup/Signup';
+import IndexRouter from './Routes/Route';
 
 function App() {
   return (
     <>
-    <Signup />
+    <Signup/>
+   {/* <IndexRouter /> */}
     </>
   );
 }
