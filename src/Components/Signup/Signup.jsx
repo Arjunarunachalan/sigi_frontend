3a045f0f8c48763faed9326c33a26f32a8c2cf78
Arjun <arju04551@gmail.com>
2023-04-27 10:48:42 +0530

    sighn up form completed

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -5,9 +5,9 @@ const Signup = () => {
   return (
     <>
      <div className="bg-slate-200 w-full h-screen flex justify-center items-center">
-          <div className="form-container bg-white p-8 rounded-[10px] shadow-lg ">
-            <form className="w-" >
-              <h1 className="text-4xl text-gray-900 dark:text-white font-bold text-center">Sighn Up</h1>
+          <div className="w-[400px] bg-white p-[20px] rounded-[10px] shadow-lg ">
+            <form className="" >
+              <h1 className="text-4xl text-gray-900 dark:text-white font-bold text-center">Sign Up</h1>
               <div className="mb-4">
                 <label htmlFor="" className="label">Name</label>
                 <input type="text" name="name" id="name" className="input" />
@@ -25,18 +25,18 @@ const Signup = () => {
               </div>
               <div className="mb-4">
                 <label htmlFor="cnfpassword" className="label">ConfirmPassword</label>
-                <input type="text" name="cnfpassword" id="cnfpassword" className="input" />
+                <input type="password" name="cnfpassword" id="cnfpassword" className="input" />
                 <span className="cnfpassworderr" id="cnfpassworderr"></span>
               </div>
-              <div className="mb-4">
-                <input type="checkbox" name="role" id="role" className="input" />
-                <label htmlFor="role" className="role ml-3">Sighnup As  Shope</label>
+              <div className="mb-4 flex">
+                <input type="checkbox" name="role" id="role"  />
+                <label htmlFor="role" className="role">Signup As Shope</label>
               </div>
               <div className="mb-4">
                 <button className="submit" id="submit">Submit</button>
               </div>
-             <div className="mb-4">
-              <a href="/">Are you Registered?</a>
+             <div className="mb-4  flex justify-center items-center">
+              <a href="/" >Are you Registered?</a>
              </div>
             </form>
     
