f938cbf8d69f919b7f41055ec99412dfe1d4010d
Arjun <arju04551@gmail.com>
2023-04-27 11:55:36 +0530

    router file added and settupped

---
+++ src/Routes/CustomerRoute.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter,Routes,Routes } from 'react-router-dom'
+
+const CustomerRoute = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CustomerRoute
